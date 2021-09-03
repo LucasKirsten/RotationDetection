065b54c7bea81de819a7fa4b5a832b9739a52c14
LucasKirsten <lucas.nedel.kirsten@hp.com>
2021-09-03 14:10:53 -0300

    updates on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 cd ./libs/utils/cython_utils
-rm *.so
-rm *.c
-rm *.cpp
 python setup.py build_ext --inplace
 
 cd ./libs/utils/
-rm *.so
-rm *.c
-rm *.cpp
 python setup.py build_ext --inplace
